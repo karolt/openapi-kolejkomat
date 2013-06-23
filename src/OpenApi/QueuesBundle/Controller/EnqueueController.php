4a1cad8f24814717c9744497b612603651ae1cdd
Karol Traczykowski <ktraczykowski@gmail.com>
2013-06-23 22:21:55 +0200

    zaczatki odbierania wiadomosci Ussd

---
+++ src/OpenApi/QueuesBundle/Controller/EnqueueController.php
@@ -0,0 +1,59 @@
+<?php
+namespace OpenApi\QueuesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use OpenApi\QueuesBundle\Entity\Queue;
+use OpenApi\QueuesBundle\Entity\QueueingCustomer;
+use OpenApi\QueuesBundle\Entity\Customer;
+use Symfony\Component\HttpFoundation\Request;
+
+class EnqueueController extends Controller
+{
+    public function enqueueAction(Request $request)
+    {
+        $from = $request->get("from");
+        $message = $request->get("msg");
+
+        //format wiadomosci to: 138*([0-9][0-9])*WIADOMOSC#
+        $messageArr = explode("*", $message);
+        $message = $messageArr[2];
+
+        if (!is_numeric($message)) {
+            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Message must be numeric");
+        }
+
+
+        /** @var $em \Doctrine\ORM\EntityManager */
+        $em = $this->getDoctrine()->getManager();
+        $queueId = $message;
+        /** @var $queue Queue */
+        $queue = $em->getRepository('QueuesBundle:Queue')->findOneById($queueId);
+
+        if (!$queue instanceof Queue) {
+            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("No such queue");
+        }
+
+
+        /** @var $em \Doctrine\ORM\EntityManager */
+        $em = $this->getDoctrine()->getManager();
+        $customer = $em->getRepository('QueuesBundle:Customer')->findOneByPhone($from);
+
+        if (!$customer instanceof Customer) {
+            $customer = new Customer();
+            $customer->setPhone($from);
+            $em->persist($customer);
+        }
+
+
+        $qc = new QueueingCustomer();
+        $qc->setCustomer($customer);
+        $qc->setJoinedAt(new \DateTime());
+        $qc->setQueue($queue);
+        $qc->setState(QueueingCustomer::STATE_WAITING);
+
+        $em->persist($qc);
+        $em->flush();
+
+
+    }
+}
