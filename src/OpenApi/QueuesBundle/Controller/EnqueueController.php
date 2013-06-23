df619b58a8b39094a75430c4fb3904b39403bba8
Karol Traczykowski <ktraczykowski@gmail.com>
2013-06-23 23:39:04 +0200

    podajemy miejsce w kolejce + obsluga bledow

--- src/OpenApi/QueuesBundle/Controller/EnqueueController.php
+++ src/OpenApi/QueuesBundle/Controller/EnqueueController.php
@@ -6,34 +6,77 @@ use OpenApi\QueuesBundle\Entity\Queue;
 use OpenApi\QueuesBundle\Entity\QueueingCustomer;
 use OpenApi\QueuesBundle\Entity\Customer;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class EnqueueController extends Controller
 {
     public function enqueueAction(Request $request)
     {
-        $from = $request->get("from");
-        $message = $request->get("msg");
+        try {
+            $from = $request->get("from");
+            $message = $request->get("msg");
+            $message = $this->parseUssdMessage($message);
 
-        //format wiadomosci to: 138*([0-9][0-9])*WIADOMOSC#
+            $queue = $this->getQueue($message);
+            $customer = $this->getOrCreateCustomer($from);
+
+            $this->checkCustomerAlreadyInQueue($queue, $customer);
+            $this->persistQueueingCustomer($customer, $queue);
+
+            /** @var $em \Doctrine\ORM\EntityManager */
+            $em = $this->getDoctrine()->getManager();
+            $queueingCustomers = $em->getRepository('QueuesBundle:QueueingCustomer')->findByQueueAndStates($queue, [QueueingCustomer::STATE_WAITING]);
+            $position = count($queueingCustomers);
+
+
+            return $this->render("QueuesBundle:YourQueue:enqueue.xml.twig", ['position' => $position]);
+
+        } catch (BadRequestHttpException $e){
+            return $this->render("QueuesBundle:YourQueue:enqueue_error.xml.twig", ['message' => $e->getMessage(), 'code' => $e->getStatusCode()]);
+        }
+
+
+
+    }
+
+    private function parseUssdMessage($message)
+    { //format wiadomosci to: 138*([0-9][0-9])*WIADOMOSC#
         $messageArr = explode("*", $message);
         $message = $messageArr[2];
 
         if (!is_numeric($message)) {
-            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Message must be numeric");
+            throw new BadRequestHttpException("Message must be numeric");
         }
+        return $message;
+    }
 
+    private function checkCustomerAlreadyInQueue($queue, $customer)
+    {
+        /** @var $em \Doctrine\ORM\EntityManager */
+        $em = $this->getDoctrine()->getManager();
+        $queueingCustomer = $em->getRepository('QueuesBundle:QueueingCustomer')->findOneBy(['queue' => $queue, 'customer' => $customer]);
+
+        if ($queueingCustomer instanceof QueueingCustomer) {
+            throw new BadRequestHttpException("customer already in queue");
+        }
+    }
+
+    private function persistQueueingCustomer($customer, $queue)
+    {
+        $qc = new QueueingCustomer();
+        $qc->setCustomer($customer);
+        $qc->setJoinedAt(new \DateTime());
+        $qc->setQueue($queue);
+        $qc->setState(QueueingCustomer::STATE_WAITING);
 
         /** @var $em \Doctrine\ORM\EntityManager */
         $em = $this->getDoctrine()->getManager();
-        $queueId = $message;
-        /** @var $queue Queue */
-        $queue = $em->getRepository('QueuesBundle:Queue')->findOneById($queueId);
+        $em->persist($qc);
+        $em->flush();
+    }
 
-        if (!$queue instanceof Queue) {
-            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("No such queue");
-        }
-
-
+    private function getOrCreateCustomer($from)
+    {
         /** @var $em \Doctrine\ORM\EntityManager */
         $em = $this->getDoctrine()->getManager();
         $customer = $em->getRepository('QueuesBundle:Customer')->findOneByPhone($from);
@@ -42,18 +85,22 @@ class EnqueueController extends Controller
             $customer = new Customer();
             $customer->setPhone($from);
             $em->persist($customer);
+            return array($em, $customer);
         }
+        return $customer;
+    }
 
+    private function getQueue($message)
+    {
+        /** @var $em \Doctrine\ORM\EntityManager */
+        $em = $this->getDoctrine()->getManager();
+        $queueId = $message;
+        /** @var $queue Queue */
+        $queue = $em->getRepository('QueuesBundle:Queue')->findOneById($queueId);
 
-        $qc = new QueueingCustomer();
-        $qc->setCustomer($customer);
-        $qc->setJoinedAt(new \DateTime());
-        $qc->setQueue($queue);
-        $qc->setState(QueueingCustomer::STATE_WAITING);
-
-        $em->persist($qc);
-        $em->flush();
-
-        return $this->render("QueuesBundle:YourQueue:enqueue.xml.twig", ['position' => 15]);
+        if (!$queue instanceof Queue) {
+            throw new BadRequestHttpException("No such queue");
+        }
+        return $queue;
     }
 }
